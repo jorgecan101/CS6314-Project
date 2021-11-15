15423a681cb47b8c300e8d9d5d8b34221d886c87
Jorge Cancino <jorge.cancino101@gmail.com>
2021-11-15 13:03:37 -0600

    started homepage and user login/registration

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//required for user login authentication
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require("passport-local").Strategy;
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -11,13 +18,27 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//login authentication
+app.use(session({ secret: 'this-is-a-secret-token' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//password config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+//mongoose
+mongoose.connect('mongodb://localhost:27017/library');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var monk = require('monk');
+var db = monk('localhost:27017/library');
+var libraryDB = db.get('books');
+var usersDB = db.get('users'); // TODO: Create users database
+var passport = require('passport');
+var Account = require('../models/account');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // Current idea is that we make the user sign-up/login first before being able to see homepage.
+  // TODO: May be a problem if user is already logged in and they want go to homepage, where homepage will redirect them to login
+  res.redirect('/login');
 });
 
+/* GET login page */
+router.get('/login', function(req, res, next) {
+  res.render('login'); // This should render the ejs file for login page
+});
+
+router.post('/login', passport.authenticate('local'), function(req,res){
+  res.redirect('/home');
+});
+
+/* GET sign-up page */
+router.get('/register', function(req, res, next) {
+  res.render('register'); // This should render the ejs file for register page
+});
+
+router.post('/register', function(req,res){
+  Account.register(new Account({email: req.body.email}), req.body.password, function(err, account){
+    if(err){
+      return res.render('register', {account: account});
+    }
+    passport.authenticate('local')(req,res, function(){
+      res.redirect('/');
+    })
+  });
+});
+
+/* GET home page */
+router.get('/home', function(req, res, next) {
+  res.render('homepage'); // This should render the ejs file for homepage
+});
+
+// TODO: Add more views that we may be able to use
+
+
 module.exports = router;
+
+
